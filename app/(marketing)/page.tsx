1cc3cad3cf730b109bda517b17bb629ab90ab193
Tianhao Zhang <zhangth1990@hotmail.com>
2024-02-28 22:14:34 -0700

    add navbar and footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
 import Link from "next/link";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 function MarketingPage() {
     return (
         <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center flex-col">
+            <div className={"flex items-center justify-center flex-col"}>
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2" />
                     No 1 task management
@@ -17,7 +25,12 @@ function MarketingPage() {
                     work forward
                 </div>
             </div>
-            <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+            <div
+                className={cn(
+                    "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+                    textFont.className
+                )}
+            >
                 Collaborate, manage projects, and reach new productivity peaks.
                 From high rises to the home office, the way your team works is
                 unique - accomplish it all with Taskify.
